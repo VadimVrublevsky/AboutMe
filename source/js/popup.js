96548232db22e1719839b09473e19b70009007b7
Vadim Vrublevsky <v.v.vrublevsky@gmail.com>
2019-09-10 10:52:19 +0300

    [feat] focus in the name input field when open popup

--- source/js/popup.js
+++ source/js/popup.js
@@ -5,11 +5,14 @@ const closePopupButton = document.querySelector('.modal-popup__close');
 const popup = document.querySelector('.modal-popup');
 const popupOverlay = document.querySelector('.modal-overlay');
 const form = document.querySelector('.form--popup');
+const fieldName = form.querySelector('.form__text--name');
+
 // open popup
 openPopupButton.addEventListener('click', (evt) => {
   evt.preventDefault();
   popup.classList.add("modal-popup__show");
   popupOverlay.classList.add("modal-overlay__show");
+  fieldName.focus();
 });
 // close popup
 closePopupButton.addEventListener('click', (evt) => {
